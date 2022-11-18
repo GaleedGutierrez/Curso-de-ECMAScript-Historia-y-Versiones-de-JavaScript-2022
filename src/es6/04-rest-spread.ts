9b7667cfed98d3475d1ca84280b9e9bda8f95011
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-11-18 16:20:14 -0300

    :white_check_mark: Clase N°7: ES6: asignación de desestructuración

---
+++ src/es6/04-rest-spread.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+// Array destructuring
+let fruit = ['apple', 'banana'];
+let [a, b, c] = fruit;
+console.log(a, b, c);
+
+
+// Object destructuring
+let user = {
+    username: 'Galeed',
+    age: 21
+}
+let {username, age} = user;
+console.log(username, age);
+
+export {}
