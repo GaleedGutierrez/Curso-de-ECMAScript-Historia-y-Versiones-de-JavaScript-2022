c7676a4a4fa7fb0d3055932d77ffb5503776af5f
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-11-24 19:27:21 -0300

    :white_check_mark: Clase N°18: ES7: exponentiation operator y array includes

---
+++ src/es7/00-exponential.ts
@@ -0,0 +1,3 @@
+const DATA = 3 ** 4;
+
+console.log(DATA);
---
+++ src/es7/01-array-includes.ts
@@ -0,0 +1,7 @@
+const NUMBERS = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+
+console.log(NUMBERS.includes(10));
+
+const NAMES = [ 'galeed', 'david', 'ada' ];
+
+console.log(NAMES.includes('Galeed'));
