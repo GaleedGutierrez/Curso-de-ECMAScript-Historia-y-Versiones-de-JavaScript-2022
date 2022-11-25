fa0a45d14cff694b1731e15f0adfd14d652cab38
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-11-24 23:29:44 -0300

    :white_check_mark: Clase N°20: ES8: string padding y trailing commas

---
+++ src/es8/02-string-padding.ts
@@ -0,0 +1,5 @@
+const STRING = 'Hello';
+
+console.log(STRING.padStart(10, '_'));
+console.log(STRING.padEnd(10, '_'));
+console.log(STRING);
---
+++ src/es8/03-trailing-commas.ts
@@ -0,0 +1,4 @@
+const AGES = [ 24, 34, 25, 24, , , , , 45 ];
+
+console.log(AGES);
+console.log(AGES.length);
