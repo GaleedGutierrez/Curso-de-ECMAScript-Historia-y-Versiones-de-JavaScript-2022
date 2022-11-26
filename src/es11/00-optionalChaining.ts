d84fbb76d6c4970f522ff22d86897885100f08db
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-11-26 03:03:22 -0300

    :white_check_mark: Clase N°27: ES11: optional chaining

---
+++ src/es11/00-optionalChaining.ts
@@ -0,0 +1,14 @@
+const USERS = {
+	galeed : {
+		country : 'AR'
+	},
+	ana : {
+		country : 'CO'
+	},
+	developer : {
+		age : 21
+	}
+};
+
+// console.log(USERS.galeed.age);
+console.log(USERS?.developer?.country);
